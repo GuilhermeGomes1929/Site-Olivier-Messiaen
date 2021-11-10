194cca7724e1c9ded3c74a89c596a0b0dde39778
Oliver Limaverde <hiperlink01@hotmail.com>
2021-11-10 17:17:58 -0300

    Adicionando CSS ao projeto

--- site-olivier-messiaen/src/components/Obra.js
+++ site-olivier-messiaen/src/components/Obra.js
@@ -2,8 +2,8 @@ export default function Obra({ id, titulo, data, instrumento, url }){
     return (
         <div class="umaObra">
             <h2 class="tituloObra">{titulo} - {data}</h2>
-            <h3 class="comentarioObra">{instrumento}</h3>
-            <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h3 class="instrumento">Para {instrumento}</h3>
+            <iframe width="280" height="150" src={"https://www.youtube.com/embed/" + url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     );
 }
